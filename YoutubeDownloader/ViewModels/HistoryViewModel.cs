ff20880adfc9e802f5a77ba3ccf2e2807c268a27
g-larose <larose.g@fromtier.com>
2023-01-05 19:09:14 -0500

    added delete history functionality

--- YoutubeDownloader/ViewModels/HistoryViewModel.cs
+++ YoutubeDownloader/ViewModels/HistoryViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Snoopy.Core.Interfaces;
+﻿using Snoopy.Core.Commands;
+using Snoopy.Core.Interfaces;
 using Snoopy.Core.Models;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Snoopy.Core.ViewModels
 {
@@ -15,18 +17,33 @@ namespace Snoopy.Core.ViewModels
         private readonly INavigator? _navigator;
         private Stack<string> _history;
 
+        public ICommand? DeleteCommand { get; set; }
+
         private ObservableCollection<History> _historyList;
         public ObservableCollection<History> HistoryList
         {
             get => _historyList;
             set => OnPropertyChanged(ref _historyList, value);
         }
+
+        private History _selectedHistory;
+        public History SelectedHistory
+        {
+            get => _selectedHistory;
+            set => OnPropertyChanged(ref _selectedHistory, value);
+        }
         public HistoryViewModel(INavigator? navigator)
         {
             _navigator = navigator;
             _history = new Stack<string>();
+            DeleteCommand = new RelayCommand(DeleteHistory);
             HistoryList= new ObservableCollection<History>();
             LoadHistory();
+        }
+
+        private void DeleteHistory()
+        {
+            var historyId = SelectedHistory;
         }
 
         private void LoadHistory()
